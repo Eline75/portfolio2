998ac5b0c3f60046fbe4b7d67e08cba27ab31b77
Eline <e.eijkelenkamp@gmail.com>
2019-03-27 15:55:43 +0100

    templates verwijderd

--- index.php
+++ index.php
@@ -1,18 +1,40 @@
 
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Portfolio-Eline</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+      integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+      crossorigin="anonymous"
+    />
+    <script src="main.js"></script>
+    <link
+      href="https://fonts.googleapis.com/css?family=Indie+Flower|Raleway"
+      rel="stylesheet"
+      />
+      <link href="https://fonts.googleapis.com/css?family=Finger+Paint" rel="stylesheet"> 
+  </head>
+
+
+<body>
 <?php
-require_once('templates/head.php'); 
 require_once("functions.php");
 ?>
- 
-<header class="index-header">
-<?php 
 
-// require_once('templates/navigation.php');
-
-require_once('templates/header.html');
-  ?>
-  </header>  
-
+<div class="index-header"></div>
 
   <section class="about">
        <div class="container">
